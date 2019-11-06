b69f505338c712c21816b6c22e9e4d6d1f64ea4a
DanilloMLS <moraesdanillo10@gmail.com>
2019-11-06 14:20:46 -0300

    Testes de feature da história de cadastro de usuário

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -11,37 +11,103 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     /**
-     * A basic feature test example.
-     *
-     * @return void
+     * Como usuário, quero me cadastrar no sistema para gerenciar meu perfil
      */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //C_01: Cadastro sem o nome
+    public function testSemNome() {
+
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => null,
+            'surname' => $user->surname,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password,
+            'cpf' => null,
+        ]);
+
+        //$response->assertRedirect(route('register'))->assertSee('Register');
+
+        $registered_user = User::where('email', $user->email)->where('name', $user->name)->first();
+        $this->assertNull($registered_user);
+
+        //$this->assertAuthenticatedAs($registered_user);
     }
 
-    public function testSemNome() {
-        $user = factory(User::class,1)->create();
-        //$user->name = null;
-        //->actingAs($user, 'guest')
-        /*
-        
-                'name' => $user->name,
-                'surname' => $user->surname,
-                'email' => $user->email,
-                'password' => $user->password,
-                'cpf' => $user->cpf,
+    //C_02: Cadastro sem sobrenome
+    public function testSemSobrenome() {
 
-                public function testBasicTest()
-                {
-                    $response = $this->get('/');
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'surname' => null,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password,
+            'cpf' => null,
+        ]);
 
-                    $response->assertStatus(200);
-                }
-         */
+        $response->assertRedirect(route('home'));
 
+        $registered_user = User::where('email', $user->email)->where('name', $user->name)->first();
+        $this->assertNotNull($registered_user);
 
+        $this->assertAuthenticatedAs($registered_user);
+    }
+
+    //C_03: Cadastro sem e-mail
+    public function testSemEmail() {
+
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'surname' => $user->surname,
+            'email' => null,
+            'password' => $user->password,
+            'password_confirmation' => $user->password,
+            'cpf' => null,
+        ]);
+
+        $registered_user = User::where('email', $user->email)->where('name', $user->name)->first();
+        $this->assertNull($registered_user);
+    }
+
+    //C_04: Cadastro sem senha
+    public function testSemSenha() {
+
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'surname' => $user->surname,
+            'email' => $user->email,
+            'password' => null,
+            'password_confirmation' => null,
+            'cpf' => null,
+        ]);
+
+        $registered_user = User::where('email', $user->email)->where('name', $user->name)->first();
+        $this->assertNull($registered_user);
+    }
+
+    //C_05: Cadastro com todas as informações
+    public function testDadosCompletos() {
+
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'surname' => $user->surname,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password,
+            'cpf' => null,
+        ]);
+
+        $response->assertRedirect(route('home'));
+
+        $registered_user = User::where('email', $user->email)->where('name', $user->name)->first();
+        $this->assertNotNull($registered_user);
+
+        $this->assertAuthenticatedAs($registered_user);
     }
 }
