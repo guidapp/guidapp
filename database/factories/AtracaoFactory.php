15b8111cf151dc1a1248bbdd9299e4ae4b369e21
Gustavo <gustavofernandezluz@gmail.com>
2019-12-05 00:52:18 -0300

    Alterando seeders para criar o banco com relação lógica

--- database/factories/AtracaoFactory.php
+++ database/factories/AtracaoFactory.php
@@ -9,7 +9,7 @@ $factory->define(Atracao::class, function (Faker $faker) {
     return [
         'nome' => $faker->name,
         'descricao' => $faker->text(200),
-        'evento_unico_id' => $faker->numberBetween(1,15),
+        'evento_unico_id' => null,
         'horario' => $faker->time(),
     ];
 });
--- database/factories/EventoFactory.php
+++ database/factories/EventoFactory.php
@@ -7,16 +7,14 @@ use App\Estabelecimento;
 use Faker\Generator as Faker;
 
 $factory->define(Evento::class, function (Faker $faker) {
-    $estabelecimento = Estabelecimento::find($faker->numberBetween(1,15));
-
     return [
         'nome' => $faker->name,
         'descricao' => $faker->text,
         'avaliacao' => $faker->numberBetween(1,5),
         'visitas' => $faker->numberBetween(0,10000),
         'hash' => $faker->text,
-        'estabelecimento_id' => $estabelecimento->id,
-        'user_id' => $estabelecimento->organizador->id,
+        'estabelecimento_id' => $faker->numberBetween(1,15),
+        'user_id' => $faker->numberBetween(1,15),
         'data' => $faker->dateTimeBetween('now','+1 year'),
         'horario' => $faker->time(),
         'tags' => $faker->text(200),
