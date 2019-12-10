b320d05bec83ba530f62574237e1fec68abc1b52
DanilloMLS <moraesdanillo10@gmail.com>
2019-12-10 14:37:46 -0300

    Remoção do link Comentários

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,9 +66,9 @@
                                   <a class="nav-link" href="{{ route('evento.cadastrar') }}">{{ __('Criar Evento') }}</a>
                           </li>
 
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                     <a class="nav-link" href="{{ route('comentarios') }}">{{ __('Comentários') }}</a>
-                            </li>
+                            </li> --}}
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
